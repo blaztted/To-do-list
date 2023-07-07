71b4b794e7100477ddc08f06b0dfc2b45a6f568d
blaztted <bbc.silva@gmail.com>
2023-07-07 19:49:50 +0100

    Set most of the Dom manipulation

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,14 +1,33 @@
 import { createToDo } from './create-to-do.js';
 import { blankProjectLoad } from './blank-project.js';
-import { initialdomManip } from './initial-dom-manip.js';
+import { displayDefaultProject, displayTheForm, addItemToCheckList, clearForm, initialdomManip } from './dom-manip.js';
+
 
 //Call blankProjectLoad on first land
 blankProjectLoad();
 
-//Call Dom Manipulation module to control UI
-initialdomManip();
+//Call DOM Manipulation module to load default project
+//displayDefaultProject();
 
-//TODO: click event module Here for todo and project creations
+//Click events module
+let clickEventsModule = (function(){
+
+    //click event for displaying the form
+    const addNewToDo = document.querySelector(".add-todo-button");
+    addNewToDo.addEventListener("click", displayTheForm);
+
+    //click event for adding an item to the checklist
+    const addToChecklist = document.querySelector(".add-to-checklist");
+    addToChecklist.addEventListener("click", addItemToCheckList);
+
+    //Click event to clear the form
+    const clearButton = document.querySelector(".reset-button");
+    clearButton.addEventListener("click", clearForm);
+})();
+
+
+
+
 
 //Call create-to-do.ks module file and apply some objects/properties
 const myToDo = createToDo("Grocery run", "Go get groceries", "07/07/2023", "Low", "Meat", "eggs");
