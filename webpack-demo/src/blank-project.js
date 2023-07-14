f675a3190e948c7053b12d3d8fca52928cd1a820
blaztted <bbc.silva@gmail.com>
2023-07-14 19:00:34 +0100

    Organized everything and started building CSS

--- webpack-demo/src/blank-project.js
+++ webpack-demo/src/blank-project.js
@@ -1,11 +1,12 @@
-//Factory function to create blank project array list
-export const blankProjectLoad = () => {
 
-    console.log("Creating blank project array now");
+// Factory function to create blank project array list
+export const blankProjectLoad = () => {
+    
+    console.log("Called blankProjectLoad module...creating blank project array now");
     let projectsArray = [];
-    console.log("Pushiing the title name of project to projectArray...");
+    console.log("Pushing the title name of project to the projectArray....");
     let projectTitle = "Default Project";
     projectsArray.push({ projectTitle });
     console.log(projectsArray);
     return { projectsArray, projectTitle };
-} 
+}
--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,42 +1,35 @@
 import { createToDo } from './create-to-do.js';
-import { blankProjectLoad } from './blank-project.js';
-import { displayDefaultProject, displayTheForm, addItemToCheckList, clearForm } from './dom-manip.js';
+import { blankProjectLoad } from './blank-project-load.js';
+import { displayDefaultProject, displayTheForm, addItemToCheckList, clearForm, displayToDo } from './dom-manip.js'
+import './style.css';
 
-
-//Call blankProjectLoad on first land
+// Call blankProjectLoad on first land
 blankProjectLoad();
 
-//TODO: COme back to this call in case I build this in the future
-//Call DOM Manipulation module to load default project
+// TODO: Come back to this call if I build out this feature in the future
+// Call DOM Manipulation module to load default project
 //displayDefaultProject();
 
-//Click events module
-let clickEventsModule = (function(){
+// Call displayToDo on first land - pulls from web local storage API (if any)
+displayToDo();
 
-    //click event for displaying the form
+// Click events module
+let clickEventsModule = (function() {
+
+    // Click event for displaying the form
     const addNewToDo = document.querySelector(".add-todo-button");
     addNewToDo.addEventListener("click", displayTheForm);
 
-    //click event for adding an item to the checklist
+    // Click event for adding an item to the checklist on the form
     const addToChecklist = document.querySelector(".add-to-checklist");
     addToChecklist.addEventListener("click", addItemToCheckList);
 
-    //Click event to clear the form
+    // Click event to clear the form
     const clearButton = document.querySelector(".reset-button");
     clearButton.addEventListener("click", clearForm);
 
-    //Click event to submit a new todo form to project
+    // Click event to submit a new todo form to project
     const submitButton = document.querySelector(".submit-button");
     submitButton.addEventListener("click", createToDo);
+    
 })();
-
-
-
-
-/* 
-//Call create-to-do.ks module file and apply some objects/properties
-const myToDo = createToDo("Grocery run", "Go get groceries", "07/07/2023", "Low", "Meat", "eggs");
-const myToDo2 = createToDo("Gym", "Go to the gym", "07/07/2023", "High", "Meat");
-
-console.log("show properties on myToDo from index.js...", myToDo);
-console.log("show properties on myToDo2 from index.js...", myToDo2); */
